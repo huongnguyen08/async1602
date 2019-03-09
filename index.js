05096b329c002c849fb61c3a8098de29423388c3
Huong <huongnguyenak96@gmail.com>
2019-03-09 14:51:04 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const URL = 'https://chain.so/api/v2/get_address_balance/BTC/';
+
+/**
+ * 1NTxf1H9PoWCUWnKcdfLtFtgGWxRmWWq83
+ * 19Li3BpAigvtv2Z9ce4B5WwjXixfwqkgVy
+ * 18cBEMRxXHqzWWCxZNtU91F5sbUNKhL5PX
+ * 1CK6KHY6MHgYvmRQ4PAafKYDrg1ejbH1cE
+ * 1H4o9Mh7HyjPa46z4vtv7J8yzaK5RY4bXR
+ */
